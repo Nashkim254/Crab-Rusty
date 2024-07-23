c775fa7c061bb67c65f847ec2570a4f2c00c00ea
programming6131 <kimutainashon@gmail.com>
2024-07-23 14:36:09 +0300

    for loops

---
+++ src/iterate.rs
@@ -0,0 +1,10 @@
+pub fn iterate(){
+let a = [10,20,30,40,50,60];
+
+for i in a.iter() {
+    println!("the value is : {}", i);
+}
+for i in 1..10{
+    print!("the value is: {}\n", i);
+}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 // mod print;
-mod vars;
+// mod vars;
+mod iterate;
 // mod types;
 // mod algo;
 // mod game;
 fn main() {
    
     // print::run();
-    vars::run();
+    // vars::run();
+    iterate::iterate();
     // game::run();
   //   let nums: [i32; 5]  = [2,4,6,8,10];
   //   algo::sumnumbers(nums);
